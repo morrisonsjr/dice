8d6e240b8935d4fa5aed5776bb2456147355cf7a
morrisonsjr <121400603+morrisonsjr@users.noreply.github.com>
2022-12-25 18:15:07 +0000

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,59 @@
+let number = 0
+input.onGesture(Gesture.Shake, function () {
+    basic.clearScreen()
+    number = randint(1, 6)
+    if (number == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (number == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . # . # .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (number == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    }
+    if (number == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    }
+    if (number == 6) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+})
